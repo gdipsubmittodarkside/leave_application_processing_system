6d56d59030dd7dcd8b2f0437777b232872f6f07f
yunting14 <lee.yunting14@gmail.com>
2022-12-14 13:40:13 +0800

    integrated yt & joey manager controller, 14Dec, 1.40pm

--- src/main/java/sg/nus/iss/team2/repository/CompensationRequestRepository.java
+++ src/main/java/sg/nus/iss/team2/repository/CompensationRequestRepository.java
@@ -1,9 +1,14 @@
 package sg.nus.iss.team2.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import sg.nus.iss.team2.model.CompensationRequest;
 
 public interface CompensationRequestRepository extends JpaRepository<CompensationRequest,Long> {
     
+    @Query("SELECT c FROM CompensationRequest c WHERE c.employee.employeeId = :emp_id AND c.status IN ('APPLIED', 'UPDATED')")
+    public List<CompensationRequest> findRequestByEmpId(Long emp_id);
 }
