25ad5f87bbc4df1c33e640b4ba936b5c37d4252a
yunting14 <lee.yunting14@gmail.com>
2022-12-16 14:04:39 +0800

    updated with leave and employee validators

--- src/main/java/sg/nus/iss/team2/model/Employee.java
+++ src/main/java/sg/nus/iss/team2/model/Employee.java
@@ -13,6 +13,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.*;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -59,8 +60,4 @@ public class Employee {
         this.status = status;
         this.managerId = managerId;
     }
-
-    
-
-    
 }
